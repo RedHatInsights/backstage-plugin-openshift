608b1fcfb5156ec19302ed5b2b7c7321cec673c6
Adam Drew <adamrdrew@live.com>
2024-07-30 17:35:32 -0400

    UI update, math and bug fixes (#6)
    * Make title simpler and more direct
    
    * Use Select for cluster selection
    
    * Clamp percentages at 100
    
    * Default to 10 rows per page
    
    * Improve tooltip legibility
    
    * Remove some cruft
    
    * Open links in new windows
    
    * Formatting and cruft removal
    
    * vscode in gitignore
    
    * Simplify graphing to just usage and requests
    
    * Formatting. Simplify colors.
    
    * Remove limits. Fix some errors.
    
    * Fix the maths and other bugs
    
    * Make replica tooltip more legible

--- plugins/openshift/src/components/DeploymentsListComponent/ResourceUsageProgress.tsx
+++ plugins/openshift/src/components/DeploymentsListComponent/ResourceUsageProgress.tsx
@@ -2,62 +2,74 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { Tooltip } from '@patternfly/react-core';
+import { Card, CardContent } from '@material-ui/core';
 
 const ResourceUsageProgress = (resourceInfo: any) => {
-    const resourceType = resourceInfo.resourceType;
-    const usage = resourceInfo.resourceUsage[resourceType];
-    const requests = resourceInfo.resourceLimitsRequests.requests ? resourceInfo?.resourceLimitsRequests?.requests[resourceType] : 0;
-    const limits = resourceInfo.resourceLimitsRequests.limits ? resourceInfo?.resourceLimitsRequests?.limits[resourceType] : 0;
+  const resourceType = resourceInfo.resourceType;
+  const usage = resourceInfo.resourceUsage[resourceType];
+  const requests = resourceInfo.resourceLimitsRequests.requests
+    ? resourceInfo?.resourceLimitsRequests?.requests[resourceType]
+    : 0;
 
-    console.log(`${resourceType} Usage: ${resourceInfo.resourceUsage[resourceType]}`)
-    console.log(`${resourceType} Limits: ${resourceInfo?.resourceLimitsRequests?.limits[resourceType]}`)
-    console.log(`${resourceType} Requests: ${resourceInfo?.resourceLimitsRequests?.requests[resourceType]}`)
+  // Bar color is green by default
+  let barColor = '#228B22';
+  // If usage is greater than 60% of the requests, but less than 90 make it yellow
+  if (usage > requests * 0.6 && usage < requests * 0.9) {
+    barColor = '#FFD700';
+  }
+  //if the usage is greater than 90% of the requests, make it red
+  if (usage > requests * 0.9) {
+    barColor = '#EE0000';
+  }
 
-    // Validate that usage is below the resource limits
-    let barColor = "#228B22";
-    if (usage > requests) {
-      barColor = usage > limits * 0.8 ? "#B22222" : "#FFD700";
+  const BorderLinearProgress = withStyles(theme => ({
+    root: {
+      height: 10,
+      borderRadius: 5,
+    },
+    colorPrimary: {
+      backgroundColor:
+        theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+    },
+    bar: {
+      borderRadius: 5,
+      backgroundColor: barColor,
+    },
+  }))(LinearProgress);
+
+  const usagePercentageOfRequests = Math.min((usage / requests) * 100, 100);
+
+  const formatResourceValue = (value: number) => {
+    if (resourceInfo.resourceType === 'memory') {
+      return `${value.toFixed(2)} Mi`;
     }
+    return `${value.toFixed(2)} cores`;
+  };
 
-    const BorderLinearProgress = withStyles((theme) => ({
-      root: {
-        height: 10,
-        borderRadius: 5,
-      },
-      colorPrimary: {
-        backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
-      },
-      bar: {
-        borderRadius: 5,
-        backgroundColor: barColor,
-      },
-    }))(LinearProgress);
-
-    const percentage = (usage / limits) * 100;
-    const usagePercentageOfRequests = (usage / requests) * 100;
-    const usagePercentageOfLimits = (usage / limits) * 100;
-    console.log(percentage)
-
-    const formatResourceValue = (value) => {
-      if (resourceInfo.resourceType === 'memory') {
-        return `${(value / 1024).toFixed(4)} GB`;
-      }
-      return `${value.toFixed(4)} cores`;
-    };
-
-    const tooltipContent = `
-    Usage: ${formatResourceValue(usage)} (${usagePercentageOfLimits.toFixed(2)}% of limits, ${usagePercentageOfRequests.toFixed(2)}% of requests)
-    Requests: ${formatResourceValue(requests)}
-    Limits: ${formatResourceValue(limits)}
-  `;
-
+  const ToolTipContent = () => {
     return (
-        <React.Fragment>
-          <Tooltip content={tooltipContent}>
-            <BorderLinearProgress variant="determinate" value={percentage} />
-          </Tooltip>
-        </ React.Fragment>
-    )
-}
+      <Card>
+        <CardContent>
+          <React.Fragment>
+            Requested: {formatResourceValue(requests)}
+            <br />
+            Used: {formatResourceValue(usage)}
+          </React.Fragment>
+        </CardContent>
+      </Card>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <Tooltip content={<ToolTipContent />}>
+        <BorderLinearProgress
+          variant="determinate"
+          value={usagePercentageOfRequests}
+        />
+      </Tooltip>
+    </React.Fragment>
+  );
+};
 
 export default ResourceUsageProgress;
--- plugins/openshift/src/components/OpenshiftComponent/OpenshiftComponent.tsx
+++ plugins/openshift/src/components/OpenshiftComponent/OpenshiftComponent.tsx
@@ -1,145 +1,166 @@
 import React, { useEffect, useState } from 'react';
 import {
-    Button,
-    ButtonGroup,
-    Typography } from '@material-ui/core';
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import { InfoCard } from '@backstage/core-components';
+import QueryQontract from '../../common/QueryAppInterface';
 import {
-    InfoCard,
-} from '@backstage/core-components';
-import QueryQontract from '../../common/QueryAppInterface'
-import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+  makeStyles,
+} from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { NSQuery } from './query';
 
 import { DeploymentsListComponent } from '../DeploymentsListComponent/DeploymentsListComponent';
 
+const clusterMap = {
+  // crc-eph is only used for testing purposes
+  'crc-eph': {
+    url: `https://console-openshift-console.apps.crc-eph.r9lp.p1.openshiftapps.com/k8s/ns`,
+    name: 'ephemeral',
+  },
+  crcs02ue1: {
+    url: `https://console-openshift-console.apps.crcs02ue1.urby.p1.openshiftapps.com/k8s/ns`,
+    name: 'stage',
+  },
+  crcp01ue1: {
+    url: `https://console-openshift-console.apps.crcp01ue1.o9m8.p1.openshiftapps.com/k8s/ns`,
+    name: 'prod',
+  },
+};
+
 export const OpenshiftComponent = () => {
-    const { result: qontractResult, loaded: qontractLoaded, error: qontractError } = QueryQontract(NSQuery);
+  const {
+    result: qontractResult,
+    loaded: qontractLoaded,
+    error: qontractError,
+  } = QueryQontract(NSQuery);
 
-    console.log(qontractResult)
+  const [currentEnvironment, setCurrentEnvironment] = useState<string>('');
+  const [currentEnvironmentUrl, setCurrentEnvironmentUrl] =
+    useState<string>('');
 
-    // state variables for stage/prod buttons
-    const [isStageButtonDisabled, setIsStageButtonDisabled] = useState<boolean>(true);
-    const [isProdButtonDisabled, setIsProdButtonDisabled] = useState<boolean>(false);
-    const [currentEnvironment, setCurrentEnvironment] = useState<string>("")
-    const [currentEnvironmentUrl, setCurrentEnvironmentUrl] = useState<string>("")
+  const getEnvironmentNamespace = (environment: string) => {
+    const clusterInfo = qontractResult.find((e: { path: string | string[]; }) =>
+      e.path.includes(`${environment}.yml`),
+    );
+    const namespaceName = clusterInfo?.name;
 
-    const getEnvironmentNamespace = (environment: string) => {
-        const clusterInfo = qontractResult.find(e => e.path.includes(`${environment}.yml`))
-        const namespaceName = clusterInfo?.name
+    return namespaceName;
+  };
 
-        return namespaceName
+  const getClusterName = (cluster: string) => {
+    if (clusterMap[cluster as keyof typeof clusterMap]) {
+      return clusterMap[cluster as keyof typeof clusterMap].name;
     }
+    return cluster;
+  };
 
-    const clusterMap = {
-        // crc-eph is only used for testing purposes
-        'crc-eph': {
-            url: `https://console-openshift-console.apps.crc-eph.r9lp.p1.openshiftapps.com/k8s/ns`,
-            name: 'ephemeral',
-        },
-        crcs02ue1: {
-            url: `https://console-openshift-console.apps.crcs02ue1.urby.p1.openshiftapps.com/k8s/ns`,
-            name: 'stage',
-        },
-        crcp01ue1: {
-            url: `https://console-openshift-console.apps.crcp01ue1.o9m8.p1.openshiftapps.com/k8s/ns`,
-            name: 'prod',
-        },
-    };
-
-    const getClusterName = (cluster: string) => {
-        if (clusterMap[cluster as keyof typeof clusterMap]) {
-            return clusterMap[cluster as keyof typeof clusterMap].name;
-        }
-        return cluster;
-    };
-
-    const getClusterUrl = (cluster: string) => {
-        if (clusterMap[cluster as keyof typeof clusterMap]) {
-            return clusterMap[cluster as keyof typeof clusterMap].url;
-        }
-        return cluster;
-    };
-    const capitalizeFirstLetter = (currentEnvironment: string) => {
-        return currentEnvironment.charAt(0).toUpperCase() + currentEnvironment.slice(1);
+  const getClusterUrl = (cluster: string) => {
+    if (clusterMap[cluster as keyof typeof clusterMap]) {
+      return clusterMap[cluster as keyof typeof clusterMap].url;
     }
-
-    const title: string = `Deployment Information - ${capitalizeFirstLetter(currentEnvironment)} cluster`;
-    const namespaceName = getEnvironmentNamespace(currentEnvironment)
-
-    // styles for linear progress bar
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          width: '100%',
-          '& > * + *': {
-            marginTop: theme.spacing(2),
-          },
-        },
-    }));
-
-    const theme = createTheme({
-        palette: {
-          action: {
-            disabledBackground: '#191970',
-            disabled: 'set color of text here'
-          }
-        }
-      }
-    )
-
-    const classes = useStyles();
-
-    const buttonHandler = (isStageDisabled: boolean, isProdDisabled: boolean) => {
-        setIsStageButtonDisabled(isStageDisabled)
-        setIsProdButtonDisabled(isProdDisabled)
-
-        setCurrentEnvironment(isProdButtonDisabled ? getClusterName('crcs02ue1') : getClusterName('crcp01ue1'))
-        setCurrentEnvironmentUrl(isProdButtonDisabled ? getClusterUrl('crcs02ue1') : getClusterUrl('crcp01ue1'))
-    }
-
-    const ClusterButtons = () => {
-        return (
-            <ThemeProvider theme={theme}>
-                <Button size="small" variant="contained" color="primary" onClick={() => buttonHandler(true, false)} disabled={isStageButtonDisabled}>Stage</Button>
-                <Button size="small" variant="contained" color="primary" onClick={() => buttonHandler(false, true)} disabled={isProdButtonDisabled}>Prod</Button>
-            </ThemeProvider>
-        );
-    }
-
-    // On page load set the current cluster to stage
-    useEffect(() => {
-        const currentClusterName = getClusterName('crcs02ue1')
-        const currentClusterUrl = getClusterUrl('crcs02ue1')
-
-        setCurrentEnvironment(currentClusterName)
-        setCurrentEnvironmentUrl(currentClusterUrl)
-    }, [])
-
-    if (qontractError) {
-        return (
-            <InfoCard>
-                <Typography align="center" variant="body1">
-                    Error retrieving data from qontract.
-                </Typography>
-            </InfoCard>
-        )
-    }
-
-    if (!qontractLoaded) {
-        return (
-            <InfoCard className={classes.root} title={title}>
-                <LinearProgress />
-            </InfoCard>
-        )
-    }
-
+    return cluster;
+  };
+  const capitalizeFirstLetter = (currentEnvironment: string) => {
     return (
-        <InfoCard title={title}>
-            <div style={{marginBottom: '20px'}}>
-                <ClusterButtons />
-            </div>
-            <DeploymentsListComponent key={currentEnvironment} environmentName={currentEnvironment} namespace={namespaceName} environmentUrl={currentEnvironmentUrl} qontractResult={qontractResult} />
-            
-        </InfoCard>
-    )
-}
+      currentEnvironment.charAt(0).toUpperCase() + currentEnvironment.slice(1)
+    );
+  };
+
+  const title: string = `${capitalizeFirstLetter(
+    currentEnvironment,
+  )} Deployments`;
+  const namespaceName = getEnvironmentNamespace(currentEnvironment);
+
+  // styles for linear progress bar
+  const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
+  const ClusterSelect = () => {
+    return (
+      <FormControl>
+        <InputLabel id="cluster-select-label">Cluster</InputLabel>
+        <Select
+          labelId="cluster-select-label"
+          id="cluster-select"
+          value={currentEnvironment}
+          onChange={e => {
+            setCurrentEnvironment(e.target.value as string);
+            setCurrentEnvironmentUrl(
+              e.target.value === 'stage'
+                ? getClusterUrl('crcs02ue1')
+                : getClusterUrl('crcp01ue1'),
+            );
+          }}
+        >
+          <MenuItem value={'stage'}>Stage</MenuItem>
+          <MenuItem value={'prod'}>Prod</MenuItem>
+        </Select>
+      </FormControl>
+    );
+  };
+
+  const InfoCardTitle = () => {
+    return (
+      <Grid container direction="row">
+        <Grid item xs={11}>
+          {title}
+        </Grid>
+        <Grid item xs={1}>
+          <ClusterSelect />
+        </Grid>
+      </Grid>
+    );
+  };
+
+  // On page load set the current cluster to stage
+  useEffect(() => {
+    const currentClusterName = getClusterName('crcs02ue1');
+    const currentClusterUrl = getClusterUrl('crcs02ue1');
+    setCurrentEnvironment(currentClusterName);
+    setCurrentEnvironmentUrl(currentClusterUrl);
+  }, []);
+
+  if (qontractError) {
+    return (
+      <InfoCard>
+        <Typography align="center" variant="body1">
+          Error retrieving data from qontract.
+        </Typography>
+      </InfoCard>
+    );
+  }
+
+  if (!qontractLoaded) {
+    return (
+      <InfoCard className={classes.root} title={title}>
+        <LinearProgress />
+      </InfoCard>
+    );
+  }
+
+  return (
+    <InfoCard title={<InfoCardTitle />}>
+      <DeploymentsListComponent
+        key={currentEnvironment}
+        environmentName={currentEnvironment}
+        namespace={namespaceName}
+        environmentUrl={currentEnvironmentUrl}
+        qontractResult={qontractResult}
+      />
+    </InfoCard>
+  );
+};
